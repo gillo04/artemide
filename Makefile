b41363dea27179d64bb63acf8f476f11f2ac42e5
gillo04 <gvsgames.mail@gmail.com>
2024-03-12 20:12:04 +0100

    Implemented new way of dealing with the circuit graph

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 all:
 	g++ *.cpp -o artemide -lraylib -lglfw -lGL -lm -lpthread -ldl -lrt -lX11 -I~/raylib/build/raylib/include/
 	./artemide
+
+valgrind:
+	g++ -g -Og *.cpp -o artemide -lraylib -lglfw -lGL -lm -lpthread -ldl -lrt -lX11 -I~/raylib/build/raylib/include/
+	valgrind ./artemide 
+	# valgrind --leak-check=full ./artemide 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 #include <stdio.h>
 
 #include "raylib.h"
@@ -34,124 +35,63 @@ int get_snap(vector<Vector2> &points, int ignore) {
     return -1;
 }
 
-void print_graph(char graph[MAX_NODES*2][MAX_NODES*2], int point_c) {
-    for (int i = 0; i < point_c; i++) {
-        for (int j = 0; j < point_c; j++) {
-            printf("%d ", graph[i][j]);
+typedef vector<vector<tuple<int, int>>> AdjList;
+AdjList adjacency_list(vector<Vector2> &points, vector<Node> &nodes) {
+    AdjList adj_list(points.size());
+    for (int i = 0; i < nodes.size(); i++) {
+        if (nodes[i].a == -1) {
+            continue;
         }
-        printf("\n");
+        adj_list[nodes[i].a].push_back({nodes[i].b, i});
+        adj_list[nodes[i].b].push_back({nodes[i].a, i});
+    }
+    return adj_list;
+}
+
+vector<int> get_connected(int p, AdjList &adj_list, vector<Node> &nodes, vector<bool> &visited) {
+    vector<int> connected(0);
+    if (visited[p]) {
+        return connected;
+    }
+    visited[p] = true;
+
+    for (int i = 0; i < adj_list[p].size(); i++) {
+        if (nodes[get<1>(adj_list[p][i])].type == C_WIRE) {
+            vector<int> tmp = get_connected(get<0>(adj_list[p][i]), adj_list, nodes, visited);
+            connected.insert(connected.end(), tmp.begin(), tmp.end());
+        } else {
+            connected.push_back(get<1>(adj_list[p][i]));
+        }
     }
 }
 
-/*int find_node(int a, int b, Node nodes[], int node_c) {
-    for (int i = 0; i < node_c; i++) {
-        if (nodes[i].a == a && nodes[i].b == b) {
-            return i;
+void s_resistor_series(vector<Vector2> &points, vector<Node> &nodes) {
+    AdjList adj_list = adjacency_list(points, nodes);
+    for (int i = 0; i < points.size(); i++) {
+        vector<bool> visited(points.size(), false);
+        vector<int> conn = get_connected(i, adj_list, nodes, visited);
+
+        for (int j = 0; j < conn.size(); j++) {
+            cout << conn[j] << " ";
         }
-        if (nodes[i].a == b && nodes[i].b == a) {
-            return i;
-        }
-    }
-}*/
+        cout << endl;
 
-vector<Node*> get_close_a(int n, vector<Node> &nodes) {
-    vector<Node*> close;
-    int close_c = 0;
-    int pt = nodes[n].a;
-    for (int i = 0; i < nodes.size(); i++) {
-        if (nodes[i].a == -1 || i == n) {
-            continue;
-        }
+        if (conn.size() == 2) {
+            if (nodes[conn[0]].type == C_RESISTOR &&
+                nodes[conn[1]].type == C_RESISTOR) { // TODO: soon to remove
 
-        if (nodes[i].a == pt || nodes[i].b == pt) {
-            close.push_back(&nodes[i]);
-        }
-    }
-    return close;
-}
-
-vector<Node*> get_close_b(int n, vector<Node> &nodes) {
-    vector<Node*> close;
-    int close_c = 0;
-    int pt = nodes[n].b;
-    for (int i = 0; i < nodes.size(); i++) {
-        if (nodes[i].a == -1 || i == n) {
-            continue;
-        }
-
-        if (nodes[i].a == pt || nodes[i].b == pt) {
-            close.push_back(&nodes[i]);
-        }
-    }
-    return close;
-}
-
-void simplify_resistor_series(vector<Vector2> &points,
-                              vector<Node> &nodes, 
-                              char graph[MAX_NODES*2][MAX_NODES*2]) {
-    for (int i = 0; i < nodes.size(); i++) {
-        if (nodes[i].a == -1 || nodes[i].type != C_RESISTOR) {
-            continue;
-        }
-        // Case a
-        {
-            vector<Node*> close = get_close_a(i, nodes);
-
-            int current_pt = nodes[i].a;
-            if (close.size() == 1) {
-                if (close[0]->type == C_RESISTOR) {
-                    int last_conn = (close[0]->a == current_pt)
-                                    ? close[0]->b
-                                    : close[0]->a;
-
-                    graph[current_pt][last_conn] = 0;
-                    graph[last_conn][current_pt] = 0;
-
-                    graph[close[0]->a][close[0]->b] = 0;
-                    graph[close[0]->b][close[0]->a] = 0;
-
-                    nodes[i].a = last_conn;
-                    close[0]->a = -1;
-                    close[0]->b = -1;
-
-                    nodes[i].value += close[0]->value;
-                }
+                nodes[conn[0]].value += nodes[conn[1]].value;
+                nodes[conn[1]].value = -1;
+                nodes[conn[1]].type = C_WIRE;
             }
         }
 
-        // Case b
-        {
-            vector<Node*> close = get_close_b(i, nodes);
-
-            int current_pt = nodes[i].b;
-            if (close.size() == 1) {
-                if (close[0]->type == C_RESISTOR) {
-                    int last_conn = (close[0]->a == current_pt)
-                                    ? close[0]->b
-                                    : close[0]->a;
-
-                    graph[current_pt][last_conn] = 0;
-                    graph[last_conn][current_pt] = 0;
-
-                    graph[close[0]->a][close[0]->b] = 0;
-                    graph[close[0]->b][close[0]->a] = 0;
-
-                    nodes[i].b = last_conn;
-                    close[0]->a = -1;
-                    close[0]->b = -1;
-
-                    nodes[i].value += close[0]->value;
-                }
-            }
-        }
     }
 }
 
 int main() {
     vector<Vector2> points;
     vector<Node> nodes;
-
-    char graph[MAX_NODES*2][MAX_NODES*2];
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Artemide");
 
@@ -174,10 +114,6 @@ int main() {
                         .a = snap,
                         .b = points.size()-1
                     });
-                    // points[point_c] = GetMousePosition();       // b
-                    /*nodes[node_c].a = snap;
-                    nodes[node_c].b = point_c;
-                    point_c += 1;*/
                 } else {
                     points.push_back(GetMousePosition());
                     points.push_back(GetMousePosition());
@@ -185,15 +121,7 @@ int main() {
                         .a = points.size()-2,
                         .b = points.size()-1
                     });
-                    /*points[point_c] = GetMousePosition();       // a
-                    points[point_c+1] = GetMousePosition();     // b
-                    nodes[node_c].a = point_c;
-                    nodes[node_c].b = point_c+1;
-                    point_c += 2;*/
                 }
-                // Convert these lines to work with the nodes vector
-                // nodes[node_c].type = current_component;
-                // nodes[node_c].value = node_c * 5;
 
                 nodes.back().type = current_component;
                 nodes.back().value = nodes.size() * 5;
@@ -207,10 +135,8 @@ int main() {
 
             if (current_component != C_SELECT) {
                 if (snap != -1) {
-                    // point_c -= 1;
                     points.pop_back();
                 }
-                // node_c++;
             }
         }
 
@@ -225,21 +151,8 @@ int main() {
                 current_component = C_RESISTOR;
                 break;
             case KEY_SPACE:
-                printf("Converting to graph\n");
-
-                for (int i = 0; i < points.size(); i++) {
-                    for (int j = 0; j < points.size(); j++) {
-                        graph[i][j] = 0;
-                    }
-                }
-
-                for (Node node : nodes) {
-                    graph[node.b][node.a] = 1;
-                    graph[node.a][node.b] = 1;
-                }
-
-                print_graph(graph, points.size());
-                simplify_resistor_series(points, nodes, graph);
+                printf("Simplifying\n");
+                s_resistor_series(points, nodes);
                 break;
             default:
                 break;
